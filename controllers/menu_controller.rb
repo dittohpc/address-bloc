08b6e71b42affb85cbd960b001f1894de9026fb8
enoughtime@gmail.com <enoughtime@gmail.com>
2016-02-10 17:26:54 -0500

    Assignment 20 menu completed

--- controllers/menu_controller.rb
+++ controllers/menu_controller.rb
@@ -15,7 +15,8 @@
      puts "2 - Create an entry"
      puts "3 - Search for an entry"
      puts "4 - Import entries from a CSV"
-     puts "5 - Exit"
+     puts "5 - View Entry Number n"
+     puts "6 - Exit"
      print "Enter your selection: "
 
  # #3
@@ -39,6 +40,10 @@
       read_csv
       main_menu
     when 5
+      system "clear"
+      view_by_entry_number
+      main_menu
+    when 6
       puts "Good-bye!"
 # #8
       exit(0)
@@ -81,6 +86,21 @@
 
     system "clear"
     puts "New entry created"
+  end
+
+  def view_by_entry_number
+    print "Enter the entry number to view: "
+    entry_number = gets.chomp.to_i
+
+    if entry_number >= @address_book.entries.count
+      puts "The entry #{entry_number} does not exist"
+      view_by_entry_number
+    else
+      puts @address_book.entries[entry_number]
+      puts "Hit <enter> to go back to MAIN"
+      gets.chomp
+      system "clear"
+    end
   end
 
   def search_entries
